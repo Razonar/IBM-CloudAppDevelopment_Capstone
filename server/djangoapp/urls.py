587a6c98dfb0b18d20c6c3927f4ef8a2c09608dd
Alvaro Diaz <razonar@outlook.com>
2022-05-19 07:41:06 -0300

    Up

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,8 +23,11 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path(route='review/<int:dealer_id>', view=views.get_dealer_details, name='dealer_reviews'),
 
     # path for add a review view
+    path(route='dealer/<int:dealer_id>/', view=views.add_review, name='add_review')
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
