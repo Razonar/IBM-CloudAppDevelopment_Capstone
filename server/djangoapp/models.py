8ff6701867b2f1d86ab164de48935a20881aa968
Alvaro Diaz <razonar@outlook.com>
2022-05-20 19:54:22 -0300

    Up

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils.timezone import now
+from django.core import serializers 
+import uuid
+import json
 
 # Create your models here.
 
@@ -8,13 +11,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
 class CarMake(models.Model):
-    name = models.CharField(max_length=200, default="name")
-    description = models.TextField()
-
-    def __str__(self) -> str:
-        return super().__str__()
+    name = models.CharField(null=False, max_length=20, default='undefined')
+    description = models.TextField(null=True)
+    def __str__(self):
+        return self.name + ": " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -24,20 +25,23 @@ class CarMake(models.Model):
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
 class CarModel(models.Model):
-    name = models.CharField(max_length=200, default="name")
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    dealer_id = models.IntegerField(default=1)
+    make = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)  
+    name = models.CharField(null=False, max_length=40, default='undefined')
+    id = models.IntegerField(default=1,primary_key=True)        
+    
+    # One way -------------------
     car_type = models.CharField(max_length=200, choices=[('Sedan', 'sedan'), ('SUV', 'suv'), ('WAGON', 'wagon')])
-    year = models.DateField()
 
+    # Another way -------------------
+    TYPE_CHOICES = [('Sedan', 'sedan'), ('SUV', 'suv'), ('WAGON', 'wagon')]
+    
+    year = models.DateField()
     def __str__(self) -> str:
         return super().__str__()
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
-
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
         self.address = address
         self.city = city
@@ -49,21 +53,44 @@ class CarDealer:
         self.st = st
         self.zip = zip
         self.state = state
-
+    # - __str__ method to print a car make object
     def __str__(self):
         return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview:
-
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, id, sentiment=None):
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
         self.review = review
-        self.purchase_date = purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
-        self.sentiment = sentiment
-        self.id = id
+        # Optional attributes
+        self.purchase_date = ""
+        self.purchase_make = ""
+        self.purchase_model = ""
+        self.purchase_year = ""
+        self.sentiment = ""
+        self.id = ""
+    def __str__(self):
+        return "Review: " + self.review
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
+
+# Another way, can't make DealerReview works
+class ReviewPost:
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        # Optional attributes
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+    # - needs to_json
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
