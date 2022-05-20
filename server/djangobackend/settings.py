d35459cb3dd265eb48f4bb9275526aab4c18e171
Alvaro Diaz <razonar@outlook.com>
2022-05-20 02:47:19 -0300

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost","django-server.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["localhost","alvaro-001.mybluemix.net"]
 
 
 # Application definition
