bff5dd7e840c41c985e80e1fb4f2ab0fd881332a
Alvaro Diaz <razonar@outlook.com>
2022-05-21 16:20:24 -0300

    Up

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,14 +8,12 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
     path(route='', view=views.get_dealerships, name='index'),
-    path(route='about/', view=views.about, name='about'),
-    path(route='contact/', view=views.contact, name='contact'),
-    path(route='registration/', view=views.registration_request, name='registration'),
-    path(route='login/', view=views.login_request, name='login'),
-    path(route='logout/', view=views.logout_request, name='logout'),
-    path(route='review/<int:dealer_id>', view=views.get_dealer_details, name='dealer_reviews'),
-    path(route='dealer/<int:dealer_id>/', view=views.add_review, name='add_review')
-
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path(route='dealer/<int:id>/', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:id>/review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
